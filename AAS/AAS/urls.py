0b78067775a78ae1507937dc6243153bfe41cf52
123RUN321 <1318902153@qq.com>
2025-03-30 20:01:37 +0800

    用户注册

--- AAS/AAS/urls.py
+++ AAS/AAS/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+#1.导入系统的logging
+import logging
+# #2.创建日志器
+# logger=logging.getLogger('django')
+#
+# from django.http import HttpResponse
+#
+# def log(request):
+#     # 3.使用日志器记录信息
+#     logger.info('info')
+#     return  HttpResponse('test')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # include 参数1要设置为元组（urlconf_module, app_name）
+    # namespace 设置命名空间
+    path('', include(('users.urls', 'users'), namespace='users')),
 ]
